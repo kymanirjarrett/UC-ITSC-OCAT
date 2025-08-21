308e1a203dc27da69602530be27a81b0a055d31a
Kymani Jarrett <krj.kymani13@gmail.com>
2025-08-21 12:53:20 -0400

    Working on displaying assessment list using react table

---
+++ packages/api/src/routes/assessments.ts
@@ -0,0 +1,13 @@
+{/*
+import { Router } from 'express';
+import { container } from '../../inversify.config'; // or wherever your DI container is
+import { GetAssessmentListUseCase } from '../application/features/assessments/getList/useCase';
+import { getAssessmentListController } from '../application/features/assessments/getList/controller';
+
+const router = Router();
+const useCase = container.get(GetAssessmentListUseCase);
+
+router.get('/list', getAssessmentListController(useCase));
+
+export default router;
+*/}
